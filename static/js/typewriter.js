4fd6c19b6fdc014ee64d59032956a117b3c111c1
Felix <felix@Felixs-MacBook-Pro.local>
2024-07-17 23:06:40 +0100

    added logo, made typing more natural and stop when not in view

--- static/js/typewriter.js
+++ static/js/typewriter.js
@@ -8,48 +8,54 @@ var words = [
   'Recovery.', 'Nutrition.', 'Preparation.', 'Progression.', 'Mastery.', 'Evolution.', 'Tradition.', 'Innovation.',
   'Simplicity.', 'Complexity.', 'Variety.', 'Versatility.', 'Refinement.'
 ];
+
 var currentWordIndex = 0;
 var currentLetterIndex = 0;
-var speed = Math.floor(Math.random() * (50 - 30 + 1)) + 30;
+var typingSpeed = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+var wordPause = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
+var typingTimeout;
 
 function typeWriter() {
   if (currentWordIndex < words.length) {
-      var currentWord = words[currentWordIndex];
-      if (currentLetterIndex < currentWord.length) {
-          document.getElementById("typing").innerHTML += currentWord.charAt(currentLetterIndex);
-          currentLetterIndex++;
-          setTimeout(typeWriter, speed);
-      } else {
-          document.getElementById("typing").innerHTML += ' ';
-          currentWordIndex++;
-          currentLetterIndex = 0;
-          setTimeout(typeWriter, speed);
-      }
+    var currentWord = words[currentWordIndex];
+    if (currentLetterIndex < currentWord.length) {
+      document.getElementById("typing").innerHTML += currentWord.charAt(currentLetterIndex);
+      currentLetterIndex++;
+      typingTimeout = setTimeout(typeWriter, typingSpeed);
+    } else {
+      document.getElementById("typing").innerHTML += ' ';
+      currentWordIndex++;
+      currentLetterIndex = 0;
+      typingTimeout = setTimeout(typeWriter, wordPause);
+    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-
   const observedElement = document.getElementById("typing");
 
   const observerOptions = {
-      root: null, // Use the viewport as the container
-      rootMargin: '0px',
-      threshold: 0.1 // Trigger when 10% of the element is visible
+    root: null, // Use the viewport as the container
+    rootMargin: '0px',
+    threshold: 1.0 // Trigger when 10% of the element is visible
   };
 
   const observerCallback = (entries, observer) => {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            console.log('Element is in view!');
-              typeWriter()
-          } else {
-              console.log('Element is out of view!');
-          }
-      });
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        console.log('Element is in view!');
+        if (!typingTimeout) {
+          typeWriter();
+        }
+      } else {
+        console.log('Element is out of view!');
+        clearTimeout(typingTimeout);
+        typingTimeout = null;
+      }
+    });
   };
 
   const observer = new IntersectionObserver(observerCallback, observerOptions);
-
   observer.observe(observedElement);
 });
+
