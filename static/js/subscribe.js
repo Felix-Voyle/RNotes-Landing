2fd3418f3947b231df605d0106090234be9c6932
Felix <felix@Felixs-MacBook-Pro.local>
2024-08-03 23:39:50 +0100

    updated loading animation added icons.

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -10,10 +10,6 @@ document.getElementById("email").addEventListener("input", function(event) {
 
 document.getElementById("subscribeForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent the form from submitting the traditional way
-
-    const submitButton = document.getElementById('submitButton');
-    const buttonText = document.getElementById('buttonText');
-    buttonText.innerHTML = '<div class="loader"></div>';
     
     const email = document.getElementById("email").value;
     const responseMessage = document.getElementById("responseMessage");
@@ -27,19 +23,33 @@ document.getElementById("subscribeForm").addEventListener("submit", function(eve
         return; 
     }
 
+    const submitButton = document.getElementById('submitButton');
+    const buttonText = document.getElementById('buttonText');
+    buttonText.innerHTML = '<div class="loader"></div>';
+
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 202) {
-                buttonText.innerHTML = '<span class="status-icon">✔️</span>';
-                submitButton.style.color = 'green';
+                buttonText.innerHTML = `
+    <span class="status-icon success">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+    </span>`;
                 responseMessage.textContent = "Subscription successful!";
-                responseMessage.style.color = "green";
+                responseMessage.classList.add("success");
+                submitButton.disabled = true;
             } else {
                 const jsonResponse = JSON.parse(xhr.responseText);
-                buttonText.innerHTML = '<span class="status-icon">❌</span>'; // Cross
-                submitButton.style.color = 'red';
-                responseMessage.textContent = jsonResponse.message;
-                responseMessage.style.color = "red";
+                buttonText.innerHTML = `
+    <span class="status-icon error">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+    </span>`;
+                responseMessage.textContent = "Error during sign up, please try again later.";
+                responseMessage.classList.add("error");
+                submitButton.disabled = true;
             }
         }
     };
